fadf480ca5e55feb1856b6681383befc8ee6d7ac
wacky6.AriesMBP <416707889@qq.com>
2018-01-26 00:36:58 +0800

    qr-decode: naive positive/negative scan

--- src/qr2.worker.js
+++ src/qr2.worker.js
@@ -1,11 +1,22 @@
 import {binarizeImage, locateQRInBinaryImage, extractQRFromBinaryImage, decodeQR} from 'jsqr'
 
+function invertInplace(u8) {
+    for (let i=0; i!==u8.length; ++i)
+        u8[i] = !u8[i]
+}
+
 onmessage = ev => {
     const imageData = ev.data
     const binaryImage = binarizeImage(imageData.data, imageData.width, imageData.height)
-    const location = locateQRInBinaryImage(binaryImage)
+    let location = locateQRInBinaryImage(binaryImage)
+    // try positive
     if (!location) {
-        return postMessage(['pattern not found', null])
+        // try negative
+        invertInplace(binaryImage.data)
+        location = locateQRInBinaryImage(binaryImage);
+        if (!location) {
+            return postMessage(['pattern not found', null])
+        }
     }
     const payload = extractQRFromBinaryImage(binaryImage, location);
     if (!payload) {
