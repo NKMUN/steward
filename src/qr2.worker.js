be69947466679111798e1ab543eaa01fb4a949ef
wacky6.AriesMBP <416707889@qq.com>
2018-01-29 00:31:06 +0800

    Hack: use modified jsqr
    jsQR upstream made incompatible changes, hacked to keep performance
    
    what's hacked:
      modified jsQR bundle `index.ts`, replace exports with low-level functions
    
    see module 3, replace export.default with {
      binarize,
      locate,
      extract,
      decode
    }

--- src/qr2.worker.js
+++ src/qr2.worker.js
@@ -1,27 +1,36 @@
-import {binarizeImage, locateQRInBinaryImage, extractQRFromBinaryImage, decodeQR} from 'jsqr'
+import {binarize, locate, extract, decode} from './lib/jsqr'
 
 function invertInplace(u8) {
     for (let i=0; i!==u8.length; ++i)
         u8[i] = !u8[i]
 }
 
+function getPayload(binaryImage) {
+    const location = locate(binaryImage)
+    if (!location) return null
+
+    const payload = extract(binaryImage, location)
+    if (!payload) return null
+
+    const decoded = decode(payload.matrix)
+    if (!decoded) return null
+
+    return decoded.text
+}
+
 onmessage = ev => {
     const imageData = ev.data
-    const binaryImage = binarizeImage(imageData.data, imageData.width, imageData.height)
-    let location = locateQRInBinaryImage(binaryImage)
-    // try positive
-    if (!location) {
-        // try negative
-        invertInplace(binaryImage.data)
-        location = locateQRInBinaryImage(binaryImage);
-        if (!location) {
-            return postMessage(['pattern not found', null])
-        }
+    const binaryImage = binarize(imageData.data, imageData.width, imageData.height)
+    const payloadPositive = getPayload(binaryImage)
+    invertInplace(binaryImage.data)
+    const payloadNegative = getPayload(binaryImage)
+    // console.log('+: ' + payloadPositive)
+    // console.log('-: ' + payloadNegative)
+
+    const ret = payloadPositive || payloadNegative
+    if (ret) {
+        postMessage([null, ret, null])
+    } else {
+        postMessage(['pattern not found', null])
     }
-    const payload = extractQRFromBinaryImage(binaryImage, location);
-    if (!payload) {
-        return postMessage(['code is corrupted', null])
-    }
-    const decoded = decodeQR(payload)
-    postMessage([null, decoded, location])
 }
